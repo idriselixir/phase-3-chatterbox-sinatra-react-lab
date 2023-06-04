08864dcf13f1a7deb606ef2d5e69bcbf9476b181
Idris Noor <idreesnoor150@gmail.com>
2023-06-04 04:39:28 +0300

    done

---
+++ chatterbox-server/db/migrate/20230604013203_create_messages.rb
@@ -0,0 +1,10 @@
+# db/migrate/timestamp_create_messages.rb
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.string :body
+      t.string :username
+      t.timestamps
+    end
+  end
+end
--- chatterbox-server/spec/02_controllers/application_controller_spec.rb
+++ chatterbox-server/spec/02_controllers/application_controller_spec.rb
@@ -64,3 +64,35 @@ describe ApplicationController do
   end
 
 end
+class ApplicationController < Sinatra::Base
+  set default_content_type: "application/json"
+
+  # GET /messages
+  get '/messages' do
+    messages = Message.all
+    messages.to_json
+  end
+
+  # POST /messages
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  # PATCH /messages/:id
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  # DELETE /messages/:id
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+end
